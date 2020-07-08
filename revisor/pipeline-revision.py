2304a757284ff9d5ee436e864baecc1b29b27fbc
José Caimapo <jcaimapo@falabella.cl>
2020-07-08 13:00:43 -0400

    style: run formatter

--- revisor/pipeline-revision.py
+++ revisor/pipeline-revision.py
@@ -21,23 +21,27 @@ import logging.handlers
 from gitlab_tree import GitlabTree
 import sys
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 log = logging.getLogger(__name__)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     arguments = docopt(__doc__, version='revisor v 0.0.1')
     print(arguments)
     in_file = None
-
-    tree = GitlabTree(arguments["<gitlab-url>"], arguments["--token"], arguments["--include"], arguments["--exclude"], arguments["--concurrency"], in_file=in_file)
-    log.debug("Reading projects tree from gitlab at [%s]", arguments["<gitlab-url>"])
+    ipattern = arguments["--include"] if arguments["--include"] is not None else list()
+    opattern = arguments["--exclude"] if arguments["--exclude"] is not None else list()
+    tree = GitlabTree(arguments["<gitlab-url>"], arguments["--token"],
+                      ipattern, opattern, arguments["--concurrency"], in_file=in_file, method=arguments["--method"])
+    log.debug(
+        "Reading projects tree from gitlab at [%s]", arguments["<gitlab-url>"])
     tree.load_tree()
     if tree.is_empty():
         log.fatal("The tree is empty, check your include/exclude patterns or run with more verbosity for debugging")
         sys.exit(1) 
 
-    if args.print:
+    if arguments["--format"]:
         tree.print_tree(arguments["--format"])
     else:
         tree.sync_tree(arguments["--dest"])
